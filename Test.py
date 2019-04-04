3b709d9cf53c6ac5000539ba31b180e450ade930
Mohammad Ali Yektaie <yektaie112358@gmail.com>
2019-04-04 10:17:37 -0400

    Adapted the label to vector and vector to label.

--- Test.py
+++ Test.py
@@ -19,6 +19,7 @@ from keras.optimizers import SGD
 from keras.utils.data_utils import get_file
 from keras.preprocessing import image
 import keras.callbacks
+from DataSet import static_get_phonetics_char_array
 
 TRAIN_INDEX_FILE = '/Volumes/Files/Georgetown/AdvancedMachineLearning/Project Data/DataSet/index_sample.txt'
 TRAIN_ALPHABET_FILE = '/Volumes/Files/Georgetown/AdvancedMachineLearning/Project Data/DataSet/alphabet_sample.txt'
@@ -29,73 +30,16 @@ def load_alphabet():
     file = open(TRAIN_ALPHABET_FILE, 'r')
 
     temp = file.readline()
-    return temp.split(',,')
+    _alphabet = temp.split(',,')
+    _alphabet_char_to_index = {ch: i for i, ch in enumerate(_alphabet)}
+    _alphabet_index_to_char = {i: ch for i, ch in enumerate(_alphabet)}
+
+    return _alphabet, _alphabet_char_to_index, _alphabet_index_to_char
 
 
-alphabet = load_alphabet()
+alphabet, alphabet_char_to_index, alphabet_index_to_char = load_alphabet()
 
 np.random.seed(55)
-
-
-# this creates larger "blotches" of noise which look
-# more realistic than just adding gaussian noise
-# assumes greyscale with pixels ranging from 0 to 1
-
-def speckle(img):
-    severity = np.random.uniform(0, 0.6)
-    blur = ndimage.gaussian_filter(np.random.randn(*img.shape) * severity, 1)
-    img_speck = (img + blur)
-    img_speck[img_speck > 1] = 1
-    img_speck[img_speck <= 0] = 0
-    return img_speck
-
-
-# paints the string in a random location the bounding box
-# also uses a random font, a slight random rotation,
-# and a random amount of speckle noise
-
-def paint_text(text, w, h, rotate=False, ud=False, multi_fonts=False):
-    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, w, h)
-    with cairo.Context(surface) as context:
-        context.set_source_rgb(1, 1, 1)  # White
-        context.paint()
-        # this font list works in CentOS 7
-        if multi_fonts:
-            fonts = ['Century Schoolbook', 'Courier', 'STIX', 'URW Chancery L', 'FreeMono']
-            context.select_font_face(np.random.choice(fonts), cairo.FONT_SLANT_NORMAL,
-                                     np.random.choice([cairo.FONT_WEIGHT_BOLD, cairo.FONT_WEIGHT_NORMAL]))
-        else:
-            context.select_font_face('Courier', cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-        context.set_font_size(25)
-        box = context.text_extents(text)
-        border_w_h = (4, 4)
-        if box[2] > (w - 2 * border_w_h[1]) or box[3] > (h - 2 * border_w_h[0]):
-            raise IOError('Could not fit string into image. Max char count is too large for given image width.')
-
-        # teach the RNN translational invariance by
-        # fitting text box randomly on canvas, with some room to rotate
-        max_shift_x = w - box[2] - border_w_h[0]
-        max_shift_y = h - box[3] - border_w_h[1]
-        top_left_x = np.random.randint(0, int(max_shift_x))
-        if ud:
-            top_left_y = np.random.randint(0, int(max_shift_y))
-        else:
-            top_left_y = h // 2
-        context.move_to(top_left_x - int(box[0]), top_left_y - int(box[1]))
-        context.set_source_rgb(0, 0, 0)
-        context.show_text(text)
-
-    buf = surface.get_data()
-    a = np.frombuffer(buf, np.uint8)
-    a.shape = (h, w, 4)
-    a = a[:, :, 0]  # grab single channel
-    a = a.astype(np.float32) / 255
-    a = np.expand_dims(a, 0)
-    if rotate:
-        a = image.random_rotation(a, 3 * (w - top_left_x) / w + 1)
-    a = speckle(a)
-
-    return a
 
 
 def shuffle_mats_or_lists(matrix_list, stop_ind=None):
@@ -123,8 +67,8 @@ def shuffle_mats_or_lists(matrix_list, stop_ind=None):
 # Translation of characters to unique integer values
 def text_to_labels(text):
     ret = []
-    for char in text:
-        ret.append(alphabet.find(char))
+    for char in static_get_phonetics_char_array(text):
+        ret.append(alphabet_char_to_index[char])
     return ret
 
 
@@ -135,7 +79,7 @@ def labels_to_text(labels):
         if c == len(alphabet):  # CTC Blank
             ret.append("")
         else:
-            ret.append(alphabet[c])
+            ret.append(alphabet_index_to_char[c])
     return "".join(ret)
 
 
